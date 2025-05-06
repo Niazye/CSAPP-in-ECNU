5b22aaa18688d7a2776cbcf76a18f5f31d17ee40
niazye <niazye@outlook.com>
2025-05-07 01:10:51 +0800

    finish lab4's Part A

--- lab/lab4/cachelab-handout/csim.c
+++ lab/lab4/cachelab-handout/csim.c
@@ -3,39 +3,53 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef unsigned long Ulong;
 typedef unsigned int Uint;
+typedef struct {
+	char valid;
+	Ulong tag;
+	Ulong data;
+	Uint LRU;
+} CacheLine;
 void print_help();
-void analyze_addr(Ulong addr, Ulong num_s, Ulong num_t, Ulong num_b, Ulong* tag, Ulong* index, Ulong* offset);
-int main(int argc, char* argv[]) {
+void analyze_addr(Ulong addr, Ulong num_s, Ulong num_b, Ulong* tag, Ulong* index, Ulong* offset);
+int access_data(CacheLine** cache_begin, Ulong tag, Ulong index, Uint num_E, Ulong *line);
+int main(int argc, char* argv[]) {/* 
 	argc = 9;
-	argv[0] = "./PRO";
+	argv[0] = "./csim";
 	argv[1] = "-s";
-	argv[2] = "8";
+	argv[2] = "4";
 	argv[3] = "-E";
-	argv[4] = "2";
+	argv[4] = "1";
 	argv[5] = "-b";
 	argv[6] = "4";
 	argv[7] = "-t";
-	argv[8] = "traces/yi.trace";
+	argv[8] = "traces/trans.trace"; */
 
 	Uint opt;
 	FILE* trace_file;
 	char* file_addr;
-	Uint num_s = 0, num_b = 0, num_m = 64, num_t;
-	Uint num_S, num_B, num_E = 0, num_C;
-	Uint tot_size;
-	void* cache_begin;
+	Uint num_s = 0, num_b = 0;
+	Uint num_S, num_E = 0;
+	CacheLine** cache_begin;
 	char instrtuction;
 	Ulong addr, value;
-	Ulong tag, offset, index;
+	Ulong tag;
+	Ulong index, offset;
+	Uint i, j;
+	Ulong hits = 0, misses = 0, evictions = 0;
+	Ulong instruction_count = 0, line_idx;
+	int instruction_result;
+	int verbose = 0;
 	while ((opt = getopt(argc, argv, "vhs:E:b:t:")) != -1) {
 		switch (opt) {
 			case 'h':
 				print_help();
 				exit(0);
 			case 'v':
+				verbose = 1;
 				break;
 			case 's':
 				num_s = atoi(optarg);
@@ -63,39 +77,97 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 	num_S = 1 << num_s;
-	num_B = 1 << num_b;
-	num_t = num_m - num_s - num_b;
-	num_C = num_S * num_E * num_B;
-
-	tot_size = num_S * num_E * (1 + num_t + num_B);
-	cache_begin = malloc(tot_size);
-	while(fscanf(trace_file, "%c %lx,%lx", &instrtuction, &addr, &value)) {
-		analyze_addr(addr, num_s, num_t, num_b, &tag, &offset, &index);
+	cache_begin = (CacheLine**) malloc(num_S * sizeof(CacheLine*));
+	for(i = 0; i < num_S; i++) {
+		cache_begin[i] = (CacheLine*) malloc(num_E * sizeof(CacheLine));
+		for(j = 0; j < num_E; j++) {
+			cache_begin[i][j].valid = 0;
+			cache_begin[i][j].tag = 0;
+			cache_begin[i][j].data = 0;
+			cache_begin[i][j].LRU = 0;
+		}
+	}
+	while(fscanf(trace_file, " %c %lx,%lx", &instrtuction, &addr, &value) != EOF) {
+		analyze_addr(addr, num_s, num_b, &tag, &index, &offset);
+		instruction_count++;
+		if(verbose) printf("%c %lx,%lx", instrtuction, addr, value);
 		switch(instrtuction) {
-			case 'L':
-
-				break;
-			case 'S':
-				break;
 			case 'M':
+				instruction_result = access_data(cache_begin, tag, index, num_E, &line_idx);
+				if(instruction_result == 0) {
+					if(verbose) printf(" hit");
+					hits++;
+				}
+				else if(instruction_result == 1) {
+					if(verbose) printf(" miss");
+					misses++;
+				}
+				else {
+					if(verbose) printf(" miss eviction");
+					misses++;
+					evictions++;
+				}
+			case 'L':
+			case 'S':
+				instruction_result = access_data(cache_begin, tag, index, num_E, &line_idx);
+				if(instruction_result == 0) {
+					if(verbose) printf(" hit");
+					hits++;
+				}
+				else if(instruction_result == 1) {
+					if(verbose) printf(" miss");
+					misses++;
+				}
+				else {
+					if(verbose) printf(" miss eviction");
+					misses++;
+					evictions++;
+				}
+				cache_begin[index][line_idx].LRU = instruction_count;
+				if(verbose) printf("\n");
 				break;
 			default:
 		}
 
 	}
-
-	printf("s = %d, E = %d, b = %d, trace file = %s\n", num_s, num_E, num_b, file_addr);
-    	//printSummary(0, 0, 0);
-
 	fclose(trace_file);
+
+	
+    printSummary(hits, misses, evictions);
+
 	return 0;
 }
 
-void analyze_addr(Ulong addr, Ulong num_s, Ulong num_t, Ulong num_b, Ulong* tag, Ulong* index, Ulong* offset) {
+void analyze_addr(Ulong addr, Ulong num_s, Ulong num_b, Ulong* tag, Ulong* index, Ulong* offset) {
+	*index = (addr >> num_b) & ((1 << num_s) - 1);
 	*offset = addr & ((1 << num_b) - 1);
-	*tag = (addr >> num_b) & ((1 << num_t) - 1);
-	*index = addr >> (num_b + num_t);
+	*tag = addr >> (num_b + num_s);
 	return;
+}
+int access_data(CacheLine** cache_begin, Ulong tag, Ulong index, Uint num_E, Ulong *line) {
+	int i = 0;
+	int to_evict = 0;
+	for(i = 0; i < num_E; i++) {
+		if(cache_begin[index][i].valid == 1) {
+			if(cache_begin[index][i].tag == tag) {
+				*line = i;
+				return 0;
+			}
+		}
+	}
+	for(i = 0; i < num_E; i++) {
+		if(cache_begin[index][i].LRU < cache_begin[index][to_evict].LRU) {
+			to_evict = i;
+		}
+	}
+	*line = to_evict;
+	cache_begin[index][to_evict].valid = 1;
+	cache_begin[index][to_evict].tag = tag;
+	cache_begin[index][to_evict].data = 0;
+	if(cache_begin[index][to_evict].LRU == 0) {
+		return 1;
+	}
+	return -1;
 }
 void print_help() {
 	printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
