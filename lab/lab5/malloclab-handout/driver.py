be0cae0da78f4eaf4b9ee80085410d13e7c9bf1e
niazye <niazye@outlook.com>
2025-06-09 11:17:03 +0800

    finish implict free list implementation

---
+++ lab/lab5/malloclab-handout/driver.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("make clean && make && ./mdriver -V -t traces/")
--- lab/lab5/malloclab-handout/mm.c
+++ lab/lab5/malloclab-handout/mm.c
@@ -35,36 +35,141 @@ team_t team = {
     ""
 };
 
+/* !!!! Choose on of the following two implementations: */
+
+#define EXPLICIT
+
+/* Basic constants and macros */
+#define WSIZE 4
+#define DSIZE 8
+
+#ifdef IMPLICIT
+/* !!! BEGINNING OF IMPLICIT FREE LIST IMPLEMENTATION */
+
+/* Pack a size and allocated bit into a header word */
+#define PACK(size, allocated) ((size) | (allocated))
+
+/* Read and write a word at address p */
+#define READ(p) (*(unsigned int *)(p))
+#define WRITE(p, val) (*(unsigned int *)(p) = (val))
+
+/* Read the size and allocated field from a header word */
+#define GET_SIZE(word) (READ(word) & ~0b111)
+#define GET_ALLOC(word) (READ(word) & 0b1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT(bp) ((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+
+/* Given block ptr bp, set free if para is 0 or allocated if 1 */
+#define SET(bp, val) WRITE(HDRP(bp), GET_SIZE(HDRP(bp)) | (val))
+
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+void *heap = NULL;
+void *heap_end = NULL;
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    heap = mem_sbrk(6 * WSIZE);
+    if (heap == (void *)-1)
+        return -1;
+    heap += DSIZE;
+    WRITE(HDRP(heap), PACK(6 * WSIZE, 0));
+    heap_end = (char *)heap + 2 * DSIZE;
     return 0;
 }
 
+void cut_block(void *bp, size_t size)
+{
+    size_t block_size = GET_SIZE(HDRP(bp));
+    if (block_size > size) {
+        WRITE(HDRP(bp), PACK(size, 0));
+        WRITE(HDRP(bp) + size, PACK(block_size - size, 0));
+    }
+    return;
+}
+
+int coalesce_block(void *bp)
+{
+    void *next_bp = NEXT(bp);
+    size_t block_size = GET_SIZE(HDRP(bp));
+    size_t next_blk_size = GET_SIZE(HDRP(next_bp));
+    if (GET_ALLOC(HDRP(next_bp)) || GET_ALLOC(HDRP(bp)))
+        return -1;
+    if (next_bp >= heap_end)
+        return 0;
+    
+    WRITE(HDRP(bp), PACK(block_size + next_blk_size, 0));
+    return 1;
+}
+
+/* If cannot find a suitable fit, extend the heap */
+/* and return the beginning of the new area (the same as previous brk)*/
+void *extend_heap(size_t size)
+{
+    //printf("\nbefore, heap end is %p\n", mem_heap_hi());
+    void *new_area = mem_sbrk(size);
+    //printf("after, heap end is %p\n", mem_heap_hi());
+    if (new_area == (void *)-1)
+        return NULL;
+    WRITE(new_area + WSIZE, PACK(size, 0));
+    heap_end += size;
+    return new_area;
+}
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    size = ALIGN(size + DSIZE);
+    void *currentbp = heap;
+    void *cur_blk_header = HDRP(currentbp);
+    while(1) {
+        if (currentbp >= heap_end) {
+            // void *new_area =
+            extend_heap(size);
+            //return new_area + WSIZE;
+        }
+        cur_blk_header = HDRP(currentbp);
+        if (GET_ALLOC(cur_blk_header)) {
+            currentbp = NEXT(currentbp);
+            continue;
+        }
+        if(GET_SIZE(cur_blk_header) >= size) {
+            cut_block(currentbp, size);         // 切割时把原数组的内容修改掉了所以过不了realloc
+            SET(currentbp, 1);
+            return currentbp;
+        }
+        /* While be able to coalesce && block_size can not satisfy the request */
+        while(GET_SIZE(cur_blk_header) < size) {
+            /* If cannot coalesce anymore, jump to next block; else continue coalescing */
+            switch (coalesce_block(currentbp)) {
+                case -1:
+                    /* If this or next block is allocated, jump to next block */
+                    currentbp = NEXT(currentbp);
+                    cur_blk_header = HDRP(currentbp);
+                    break;
+                case 0:
+                    /* If remaining memory is not enough, extend the heap */
+                    extend_heap(size - GET_SIZE(HDRP(currentbp)));
+                    break;
+                case 1:
+                    /* If successfully coalesced, continue coalescing */
+                    
+            }
+        }
     }
 }
 
@@ -73,6 +178,10 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    if (ptr == NULL)
+        return;
+    SET(ptr, 0);
+    return;
 }
 
 /*
@@ -80,21 +189,235 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
+    void *newptr = NULL;
+    if (ptr == NULL)
+        return mm_malloc(size);
+
+    if (size == 0)
+        return mm_free(ptr), NULL;
     
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
+    size = ALIGN(size + DSIZE);
+    if (size <= GET_SIZE(HDRP(ptr))) {
+        WRITE(HDRP(ptr), PACK(size, 1));
+        return ptr;
+    }
+
+    /* If the next block is free, try to coalesce them instead of allocating a new block */
+    /* coalesce block until it can not coalesce anymore or the block size can satisfy the request */
+    while(GET_SIZE(HDRP(ptr)) < size && coalesce_block(ptr) == 1) {
+        ;
+    }
+    if (GET_SIZE(HDRP(ptr)) >= size) {
+        cut_block(ptr, size);
+        SET(ptr, 1);
+        return ptr;
+    }
+
+    /* If reach here, it means the current block can not satisfy the request */
+    /* So, we need to allocate a new block and copy the data from the old block */
+    newptr = mm_malloc(size - DSIZE);
+    if (newptr == NULL) {
+        //SET(ptr, 1);
+        return NULL;
+    }
+    memmove(newptr, ptr, GET_SIZE(HDRP(ptr)) - DSIZE);
+    WRITE(HDRP(newptr), PACK(size, 1));
+    mm_free(ptr);
     return newptr;
+    
+}
+/* !!! END OF IMPLICIT FREE LIST IMPLEMENTATION */
+#endif
+
+#ifdef EXPLICIT
+/* !!! BEGINNING OF EXPLICIT FREE LIST IMPLEMENTATION */
+
+/* Pack a size and allocated bit into a header word */
+#define PACK(size, allocated) ((size) | (allocated))
+
+/* Read and write a word at address p */
+#define READ(p) (*(unsigned int *)(p))
+#define WRITE(p, val) (*(unsigned int *)(p) = (val))
+
+/* Read the size and allocated field from a header word */
+#define GET_SIZE(word) (READ(word) & ~0b111)
+#define GET_ALLOC(word) (READ(word) & 0b1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT(bp) ((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+
+/* Given block ptr bp, set free if para is 0 or allocated if 1 */
+#define SET(bp, val) WRITE(HDRP(bp), GET_SIZE(HDRP(bp)) | (val))
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+void *heap = NULL;
+void *heap_end = NULL;
+
+/* 
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+    heap = mem_sbrk(6 * WSIZE);
+    if (heap == (void *)-1)
+        return -1;
+    heap += DSIZE;
+    WRITE(HDRP(heap), PACK(6 * WSIZE, 0));
+    heap_end = (char *)heap + 2 * DSIZE;
+    return 0;
 }
 
+void cut_block(void *bp, size_t size)
+{
+    size_t block_size = GET_SIZE(HDRP(bp));
+    if (block_size > size) {
+        WRITE(HDRP(bp), PACK(size, 0));
+        WRITE(HDRP(bp) + size, PACK(block_size - size, 0));
+    }
+    return;
+}
+
+int coalesce_block(void *bp)
+{
+    void *next_bp = NEXT(bp);
+    size_t block_size = GET_SIZE(HDRP(bp));
+    size_t next_blk_size = GET_SIZE(HDRP(next_bp));
+    if (GET_ALLOC(HDRP(next_bp)) || GET_ALLOC(HDRP(bp)))
+        return -1;
+    if (next_bp >= heap_end)
+        return 0;
+    
+    WRITE(HDRP(bp), PACK(block_size + next_blk_size, 0));
+    return 1;
+}
+
+/* If cannot find a suitable fit, extend the heap */
+/* and return the beginning of the new area (the same as previous brk)*/
+void *extend_heap(size_t size)
+{
+    //printf("\nbefore, heap end is %p\n", mem_heap_hi());
+    void *new_area = mem_sbrk(size);
+    //printf("after, heap end is %p\n", mem_heap_hi());
+    if (new_area == (void *)-1)
+        return NULL;
+    WRITE(new_area + WSIZE, PACK(size, 0));
+    heap_end += size;
+    return new_area;
+}
+/* 
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size)
+{
+    size = ALIGN(size + DSIZE);
+    void *currentbp = heap;
+    void *cur_blk_header = HDRP(currentbp);
+    while(1) {
+        if (currentbp >= heap_end) {
+            // void *new_area =
+            extend_heap(size);
+            //return new_area + WSIZE;
+        }
+        cur_blk_header = HDRP(currentbp);
+        if (GET_ALLOC(cur_blk_header)) {
+            currentbp = NEXT(currentbp);
+            continue;
+        }
+        if(GET_SIZE(cur_blk_header) >= size) {
+            cut_block(currentbp, size);         // 切割时把原数组的内容修改掉了所以过不了realloc
+            SET(currentbp, 1);
+            return currentbp;
+        }
+        /* While be able to coalesce && block_size can not satisfy the request */
+        while(GET_SIZE(cur_blk_header) < size) {
+            /* If cannot coalesce anymore, jump to next block; else continue coalescing */
+            switch (coalesce_block(currentbp)) {
+                case -1:
+                    /* If this or next block is allocated, jump to next block */
+                    currentbp = NEXT(currentbp);
+                    cur_blk_header = HDRP(currentbp);
+                    break;
+                case 0:
+                    /* If remaining memory is not enough, extend the heap */
+                    extend_heap(size - GET_SIZE(HDRP(currentbp)));
+                    break;
+                case 1:
+                    /* If successfully coalesced, continue coalescing */
+                    
+            }
+        }
+    }
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void *ptr)
+{
+    if (ptr == NULL)
+        return;
+    SET(ptr, 0);
+    return;
+}
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ */
+void *mm_realloc(void *ptr, size_t size)
+{
+    void *newptr = NULL;
+    if (ptr == NULL)
+        return mm_malloc(size);
+
+    if (size == 0)
+        return mm_free(ptr), NULL;
+    
+    size = ALIGN(size + DSIZE);
+    if (size <= GET_SIZE(HDRP(ptr))) {
+        WRITE(HDRP(ptr), PACK(size, 1));
+        return ptr;
+    }
+
+    /* If the next block is free, try to coalesce them instead of allocating a new block */
+    /* coalesce block until it can not coalesce anymore or the block size can satisfy the request */
+    while(GET_SIZE(HDRP(ptr)) < size && coalesce_block(ptr) == 1) {
+        ;
+    }
+    if (GET_SIZE(HDRP(ptr)) >= size) {
+        cut_block(ptr, size);
+        SET(ptr, 1);
+        return ptr;
+    }
+
+    /* If reach here, it means the current block can not satisfy the request */
+    /* So, we need to allocate a new block and copy the data from the old block */
+    newptr = mm_malloc(size - DSIZE);
+    if (newptr == NULL) {
+        //SET(ptr, 1);
+        return NULL;
+    }
+    memmove(newptr, ptr, GET_SIZE(HDRP(ptr)) - DSIZE);
+    WRITE(HDRP(newptr), PACK(size, 1));
+    mm_free(ptr);
+    return newptr;
+    
+}
+
+
+
+
+/* END OF EXPLICIT FREE LIST IMPLEMENTATION */
+#endif
 
 
 
